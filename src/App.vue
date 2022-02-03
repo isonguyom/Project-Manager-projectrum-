4405dddbf64fcd3e9193d6a4f259c3f3dd4a0a9c
isonguyom <isonguyom@gmail.com>
2022-02-03 20:13:11 +0100

    create landing page and header component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+   <div>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+img {
+  max-width: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.onboarding {
+  width: 100%;
+  min-height: 100vh;
+  background: #4B7FB9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.onboarding img {
+  display: block;
+  width: 40%;
 }
 </style>
